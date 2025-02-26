b5c9e9758ccb7e30d4956da761313dded9f5b0d3
Kloshar <kloshar13@yahoo.com>
2025-02-26 17:06:49 +0300

    GetProdutNames in progress

--- Program.cs
+++ Program.cs
@@ -3,15 +3,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 string conn = builder.Configuration.GetConnectionString("DefaultConnection")!;
 conn = conn.Replace("replacement_name", Environment.MachineName);
-
 builder.Services.AddDbContext<ApContext>(options => options.UseSqlServer(conn));
-
 var app = builder.Build();
-
 app.UseDefaultFiles(); //поддержка файлов index.html по-умолчанию
 app.UseStaticFiles(); //поддержка статических файлов в папке wwwroot
 
 app.MapGet("/api/order", getOrders); //обработка GET запроса
+app.MapGet("/api/productnames", getProductNames); //обработка GET запроса
 app.MapDelete("/api/deleteOrder/{id:int}", deleteOrder); //обработка GET запроса
 
 app.Run();
@@ -29,6 +27,30 @@ List<Product> getProducts(HttpContext context, ApContext db)
     response.Headers.ContentType = "application/json; charset=utf-8";
     return db.Product.ToList();
 }
+async Task<IResult> getProductNames(HttpContext context, ApContext db)
+{
+    List<Product> products = db.Product.ToList();
+    List<ProductName> names = new List<ProductName>();
+
+    foreach(Product p in products)
+    {
+        ProductName name = new ProductName();
+        name.Title = p.Title;
+        name.Price = p.Price;
+        names.Add(name);
+    }
+
+    var articleNames = names.Distinct();
+
+    foreach(ProductName n in articleNames)
+    {
+        Console.WriteLine($"{n.Title}, {n.Price}");
+    }
+
+    Console.WriteLine(articleNames.GetType());
+
+    return Results.Empty;
+}
 async Task<IResult> deleteOrder(HttpRequest request, ApContext db)
 {
     int? id = Convert.ToInt32(request.RouteValues["id"]);
@@ -38,7 +60,6 @@ async Task<IResult> deleteOrder(HttpRequest request, ApContext db)
     await db.SaveChangesAsync();
     return Results.Json(order);
 }
-
 public class Order
 {
     public int Id { get; set; }
@@ -54,10 +75,15 @@ public class Product
     public int Amount { get; set; }
     public decimal Price { get; set; }
 }
-public class ApContext : DbContext
+public class ProductName
+{
+    public string Title { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
+    public class ApContext : DbContext
 {
     public DbSet<Order> Order { get; set; } = null!; //коллекция для таблицы заказы
-    public DbSet<Product> Product { get; set; } = null!; //коллекция для таблицы заказы
+    public DbSet<Product> Product { get; set; } = null!; //коллекция для таблицы товары
     public ApContext(DbContextOptions<ApContext> options) : base(options)
     {
         Database.EnsureCreated();
